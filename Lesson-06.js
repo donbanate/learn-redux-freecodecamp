cc55d30a885f48ffa878fb1a6d5b694a0d185fd5
lyndon-thinkdwm <lyndon@directworksmedia.com>
2019-08-13 02:26:13 +0800

    Question 06.01

--- Lesson-06.js
+++ Lesson-06.js
@@ -3,14 +3,17 @@
  */
 
 const defaultState = {
-  login: false
+  authenticated: false
 };
 
-const reducer = (state = defaultState, action) => {
+const authReducer = (state = defaultState, action) => {
   // change code below this line
   switch (action.type) {
     case "LOGIN":
-      return { login: true };
+      return { authenticated: true };
+
+    case "LOGOUT":
+      return { authenticated: false };
 
     default:
       return state;
@@ -18,10 +21,16 @@ const reducer = (state = defaultState, action) => {
   // change code above this line
 };
 
-const store = Redux.createStore(reducer);
+const store = Redux.createStore(authReducer);
 
-const loginAction = () => {
+const loginUser = () => {
   return {
     type: "LOGIN"
+  };
+};
+
+const logoutUser = () => {
+  return {
+    type: "LOGOUT"
   };
 };
