48b6506e8994aa68835dbee2498535b7b13fd5ad
lyndon-thinkdwm <lyndon@directworksmedia.com>
2019-08-13 05:48:46 +0800

    Dealing with array

--- WrappedUp-A.js
+++ WrappedUp-A.js
@@ -30,3 +30,53 @@ const decAction = () => {
 }; // define an action creator for decrementing
 
 const store = Redux.createStore(counterReducer); // define the Redux store here, passing in your reducers
+
+/**
+ * Use the Spread Operator on Arrays
+ */
+
+const immutableReducer = (state = ["Do not mutate state!"], action) => {
+  switch (action.type) {
+    case "ADD_TO_DO":
+      // don't mutate state here or the tests will fail
+      return [...state, action.todo];
+    default:
+      return state;
+  }
+};
+
+const addToDo = todo => {
+  return {
+    type: "ADD_TO_DO",
+    todo
+  };
+};
+
+const store = Redux.createStore(immutableReducer);
+
+/**
+ * Remove item from array
+ */
+const immutableReducer = (state = [0, 1, 2, 3, 4, 5], action) => {
+  switch (action.type) {
+    case "REMOVE_ITEM":
+      let newArr = [
+        ...state.filter((elem, idx) => {
+          // [1,2,3,5]
+          return idx !== action.index;
+        })
+      ];
+      return newArr;
+    default:
+      return state;
+  }
+};
+
+const removeItem = index => {
+  return {
+    type: "REMOVE_ITEM",
+    index
+  };
+};
+
+const store = Redux.createStore(immutableReducer);
