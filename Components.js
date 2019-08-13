4e5e3cef573f82860b127a7506dd7dd4833d365f
lyndon-thinkdwm <lyndon@directworksmedia.com>
2019-08-14 00:03:30 +0800

    Components

--- Components.js
+++ Components.js
@@ -53,3 +53,43 @@ class ParentComponent extends React.Component {
     );
   }
 }
+
+/**
+ * Render Nested Components
+ */
+const TypesOfFruit = () => {
+  return (
+    <div>
+      <h2>Fruits:</h2>
+      <ul>
+        <li>Apples</li>
+        <li>Blueberries</li>
+        <li>Strawberries</li>
+        <li>Bananas</li>
+      </ul>
+    </div>
+  );
+};
+
+const Fruits = () => {
+  return (
+    <div>
+      <TypesOfFruit /> // child component of Fruits
+    </div>
+  );
+};
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        <Fruits />
+      </div>
+    );
+  }
+};
