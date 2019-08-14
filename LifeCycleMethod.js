a6191d034a39fb77a281008995845e1575dbad0e
lyndon-thinkdwm <lyndon@directworksmedia.com>
2019-08-14 22:48:48 +0800

    LifeCycle method: componentDidMount()

--- LifeCycleMethod.js
+++ LifeCycleMethod.js
@@ -2,7 +2,7 @@
  * Life Cycle Methods
  */
 
-//* ComponentWillMount
+//* componentWillMount
 class MyComponent extends Component {
   // Method is called before the render
   componentWillMount() {
@@ -11,5 +11,32 @@ class MyComponent extends Component {
 
   render() {
     return <div />;
+  }
+}
+
+//* componentDidMount
+class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeUsers: 0
+    }
+  }
+  
+  // Method is called after the render
+  /**
+   * This method is mostly used for API calls
+   */
+  componentDidMount() {
+   //Mimics an API call
+   setTimeout(() => {
+     this.setState({
+      activeUsers: 234
+     })
+   }, 2000);
+  }
+
+  render() {
+    return <h1>Active Users: {this.state.activeUsers}</h1>;
   }
 }
